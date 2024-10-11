dd96f041f7aa68305f90030541199a6aacbaf5de
Damian Sue <damianbsue@gmail.com>
2024-10-11 17:02:48 +1100

    Cloud features

--- testing_folder/piCamTesting.py
+++ testing_folder/piCamTesting.py
@@ -45,15 +45,17 @@
 # cv2.destroyAllWindows()
 # picam2.stop()
 
-from picamera2 import Picamera2
+# from picamera2 import Picamera2
 import cv2
 import time
-from libcamera import ColorSpace
+# from libcamera import ColorSpace
 import os
 from datetime import datetime
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import threading
+from GoogleCloud.upload import GoogleServices
 
 def send_email_notification():
     # Email configuration
@@ -100,51 +102,63 @@ output_file = os.path.join(output_dir, f"output_video_{current_time}.mp4")
 #     os.remove(output_file)
 
 # Initialize Picamera2
-picam2 = Picamera2()
+# picam2 = Picamera2()
 
 # Configure the camera
-config = picam2.create_preview_configuration(main={"size": (1600, 900)})
-picam2.configure(config)
+# config = picam2.create_preview_configuration(main={"size": (1600, 900)})
+# picam2.configure(config)
 
-pipeline = (
-    f'appsrc ! videoconvert ! video/x-raw,format=I420 ! x264enc '
-    f'! h264parse ! mp4mux ! filesink location={output_file}'
-)
+# pipeline = (
+#     f'appsrc ! videoconvert ! video/x-raw,format=I420 ! x264enc '
+#     f'! h264parse ! mp4mux ! filesink location={output_file}'
+# )
 
 # output_file = "record_vid.mp4"
 # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
-out = cv2.VideoWriter(pipeline, cv2.CAP_GSTREAMER, 0, 30, (1600, 900))
+# out = cv2.VideoWriter(pipeline, cv2.CAP_GSTREAMER, 0, 30, (1600, 900))
 
 # Define recording parameters
 frame_rate = 30         # Frame per second
-total_seconds = 3600    # Total duration in second (1 hour)
+total_seconds = 60    # Total duration in second (1 hour)
 total_frame = frame_rate * total_seconds    # Total frames to write
 
 # Start the camera
-picam2.start()
+# picam2.start()
 
 frame_count = 0
 
 # Allow some time for the camera to adjust to the lighting condition
 # time.sleep(2)
 
+# Start the cloud session.
+gdSession = GoogleServices('pi-test', 'testing_folder\GoogleCloud\service-secrets.json')
 
 while frame_count - total_frame:
-    frame = picam2.capture_array()
+    # frame = picam2.capture_array()
 
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-    out.write(frame)
+    # out.write(frame)
     # Display the live camera feed
     # cv2.imshow("Live Cam Feed", frame)
 
     frame_count += 1
+    time.sleep(1/frame_rate)
 
     # if cv2.waitKey(1) & 0xFF == ord('q'):
     #     break
 
-out.release()
+    # Send the updated log every 30 seconds (900 frames) with frame count.
+    # This is a threaded operation to not interrupt recording.
+    if (frame_count % (frame_rate*30)) == 0:
+        gdSession.updateLog(f'{frame_count}/{total_frame} frames')
+        gdSession.uploadLog()
+
+# out.release()
 # cv2.destroyAllWindows()
-picam2.stop()
-send_email_notification()
+# picam2.stop()
+# send_email_notification()
+gdSession.updateLog('Session finished')
+gdSession.uploadLog()
+gdSession.logThread.join()
